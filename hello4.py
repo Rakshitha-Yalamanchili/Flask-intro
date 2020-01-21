0305e08a0ab8ec42c9d90c5aeea890e1c641b498
Rakshitha-Yalamanchili <46837431+Rakshitha-Yalamanchili@users.noreply.github.com>
2020-01-21 21:36:51 +0530

    upto routing

--- hello4.py
+++ hello4.py
@@ -3,5 +3,6 @@ from flask import render_template
 app=Flask(__name__)
 @app.route("/")
 def hello_world():
-	authors=["Ajay","Rohith"]
-	return render_template("index.html",authors=authors)
+	authors=["Ajay","Rohith","Priya"]
+	name="Rakshitha"
+	return render_template("index.html",name=name,authors=authors)
--- run_app.py
+++ run_app.py
@@ -1,5 +1,5 @@
 import os
-from hello4 import app
+from hello5 import app
 if __name__=='__main__':
 	app.debug=True
 	host=os.environ.get('IP','0.0.0.0')
